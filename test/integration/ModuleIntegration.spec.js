56a78e66996c48a7443abfa5f73e630efca058f8
Agustin Colchado <agustin@colchado.com>
2023-12-05 00:31:30 -0800

    Renames the test files to *.spec.js to prep for jest

---
+++ test/integration/ModuleIntegration.spec.js
@@ -0,0 +1,24 @@
+const spur = require('spur-ioc');
+
+// NEED to use require vs import to test module export for backward compatability
+const mainModule = require('../../');
+
+describe('Integration', function () {
+  describe('Main Module Integration Tests', () => {
+    beforeEach(() => {
+      this.ioc = spur.create('test-spur-common');
+      this.ioc.merge(mainModule());
+    });
+
+    it('base module dependencies are injectable', () => {
+      return this.ioc.inject((express, expressDevice, methodOverride, cookieParser, bodyParser, expressWinston) => {
+        expect(express).to.exist;
+        expect(expressDevice).to.exist;
+        expect(methodOverride).to.exist;
+        expect(cookieParser).to.exist;
+        expect(bodyParser).to.exist;
+        expect(expressWinston).to.exist;
+      });
+    });
+  });
+});
